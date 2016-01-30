04ae781be90a50beaa2183814e0efc8e9ef41d32
Max Falk <gmdfalk@gmail.com>
2016-01-31 00:21:15 +0100

    add Alt+j/k

--- tag-zsh-common/zsh/scripts/keybindings.sh
+++ tag-zsh-common/zsh/scripts/keybindings.sh
@@ -1,3 +1,6 @@
 # Based on what has already been entered at the prompt, do a history search when pressing Alt+Up/Down
 bindkey "^[[1;3A" history-beginning-search-backward
 bindkey "^[[1;3B" history-beginning-search-forward
+# Use Alt+j/k for vim-style history search
+bindkey "^[k" history-beginning-search-backward
+bindkey "^[j" history-beginning-search-forward
