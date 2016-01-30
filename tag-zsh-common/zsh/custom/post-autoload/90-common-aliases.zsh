06329aa14f35907239d37abf787b65690336e498
Max Falk <gmdfalk@gmail.com>
2016-01-30 14:59:31 +0100

    feat(zsh): add beginning-history-search to zsh

---
+++ tag-zsh-common/zsh/custom/post-autoload/90-common-aliases.zsh
@@ -0,0 +1,1 @@
+../post/common-aliases.zsh
---
+++ tag-zsh-common/zsh/custom/post/keybindings.zsh
@@ -0,0 +1,3 @@
+# Based on what has already been entered at the prompt, do a history search when pressing Alt+Up/Down
+bindkey "^[[1;3A" history-beginning-search-backward
+bindkey "^[[1;3B" history-beginning-search-forward
