40df6c58fb8e256046b9161ef6c07e854a6c2604
Max Falk <gmdfalk@gmail.com>
2016-03-01 21:39:09 +0100

    add mplayer to detachments

--- tag-bash-common/bash/scripts/detachments.sh
+++ tag-bash-common/bash/scripts/detachments.sh
@@ -2,7 +2,7 @@
 
 # Detach these processes from shell by default
 if have q; then
-    declare -a processes=(firefox thunderbird eog spotify hexchat vlc geany gedit medit gvim pcmanfm)
+    declare -a processes=(firefox thunderbird eog spotify hexchat vlc geany gedit medit gvim pcmanfm mplayer smplayer)
     for process in ${processes[@]}; do
         alias $process="q $process"
     done
