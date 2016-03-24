95f4691d80915264265bd8cdb850f278bab9c911
Max Falk <gmdfalk@gmail.com>
2016-03-24 19:50:16 +0100

    proxy refactoring

--- tag-bash-common/bash/scripts/proxy.sh
+++ tag-bash-common/bash/scripts/proxy.sh
@@ -1,31 +1,41 @@
 #!/usr/bin/env bash
 
+PROXY_VARS=("http_proxy" "ftp_proxy" "https_proxy" "all_proxy" "HTTP_PROXY" "HTTPS_PROXY" "FTP_PROXY" "ALL_PROXY")
+NO_PROXY_VARS=("no_proxy" "NO_PROXY")
+DEFAULT_PROXY="idproxy.id.dvag.com:3128"
+DEFAULT_NO_PROXY="localhost,127.0.0.1,dvag.com,dvag.net"
+
 export_proxy(){
-    local proxy_vars=(http_proxy ftp_proxy https_proxy all_proxy HTTP_PROXY HTTPS_PROXY FTP_PROXY ALL_PROXY)
-    local no_proxy_vars=(no_proxy NO_PROXY)
-    for env_var in "${proxy_vars[@]}"; do
-        export ${env_var}="$1"
-    done
-    for env_var in "${no_proxy_vars[@]}"; do
-        export ${env_var}="$2"
-    done
+    set_variables "$1" "${PROXY_VARS[@]}"
+    set_variables "$2" "${NO_PROXY_VARS[@]}"
 }
 
 proxy_on() {
     local proxy="$1"
-    [[ -z "${proxy}" ]] && proxy="http://idproxy.id.dvag.com:3128"
     local no_proxy="$2"
-    [[ -z "${no_proxy}" ]] && no_proxy="localhost,127.0.0.1,dvag.com,dvag.net"
+    [[ -z "${proxy}" ]] && proxy="${DEFAULT_PROXY}"
+    [[ -z "${no_proxy}" ]] && no_proxy="${DEFAULT_NO_PROXY}"
     export_proxy "${proxy}" "${no_proxy}"
 }
 
 proxy_off() {
-  export_proxy ""  
+    export_proxy
 }
 
-proxy(){
-    local proxy_vars=(http_proxy ftp_proxy https_proxy all_proxy HTTP_PROXY HTTPS_PROXY FTP_PROXY ALL_PROXY)
-    for env_var in "${proxy_vars[@]}"; do
-        [[ "${ZSH_VERSION}" ]] && echo "${env_var}: ${(P)env_var}" || echo "${env_var}: ${!env_var}"
+proxy() {  # print proxy settings
+    print_variables "${PROXY_VARS[@]}"
+    print_variables "${NO_PROXY_VARS[@]}"
+}
+
+set_variables() {
+    local value="$1"
+    shift
+    for env_var in "$@"; do
+        export "${env_var}"="$value"
+    done
+}
+print_variables() {
+    for var in "$@"; do
+        [[ "${ZSH_VERSION}" ]] && echo "${var}: ${(P)var}" || echo "${var}: ${!var}"
     done
 }
