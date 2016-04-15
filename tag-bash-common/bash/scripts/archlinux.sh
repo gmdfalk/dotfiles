662955441551c1767ec37b147a98bd5fe835c3f1
Max Falk <gmdfalk@gmail.com>
2016-04-15 17:42:50 +0200

    get PKGBUILDs and sign commits by default

--- tag-bash-common/bash/scripts/archlinux.sh
+++ tag-bash-common/bash/scripts/archlinux.sh
@@ -8,6 +8,7 @@ done
 
 # {{{ Pacman
 alias pac="pacman"
+pacget() { ${_SUDO} abs && cd "$(find /var/abs -type d -iname "$@")"; } # Update abs and cd to PKGBUILD of a package.
 alias pacin="${_SUDO} pacman -S"    # Install specific package(s) from the repositories
 alias pacind="${_SUDO} pacman -S --asdeps" # Install given package(s) as dependencies of another package
 alias pacins="${_SUDO} pacman -U"   # Install specific package not from the repositories but from a file
@@ -30,6 +31,7 @@ paccheck() { [[ "$#" == 0 ]] && pacman -Qk | grep -v "0 missing" || pacman -Qk "
 # {{{ Yaourt/AUR Helper
 if [[ "${_have_yaourt}" ]]; then
     alias ya="yaourt"
+    alias yaget="yaourt -G"     # Get PKGBUILD of package.
     alias yaconf="yaourt -C"    # Fix all configuration files with vimdiff
     alias yasu="yaourt --sucre" # Same as yaourt -Syua, but without confirmation
     alias yain="yaourt -S"      # Install specific package(s) from the repositories
@@ -53,6 +55,7 @@ fi
 # {{{ Pacaur
 if [[ "${_have_pacaur}" ]]; then
     alias pa="pacaur"
+    alias paget="pacaur -d"     # Get PKGBUILD of package.
     alias pain="pacaur -S"      # Install specific package(s) from the repositories
     alias pains="pacaur -U"     # Install specific package not from the repositories but from a file
     alias pare="pacaur -R"      # Remove the specified package(s), retaining its configuration(s) and required dependencies
