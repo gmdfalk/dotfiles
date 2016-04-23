18b575fcf21b5da80b99f33bca98166826163556
Max Falk <gmdfalk@gmail.com>
2016-04-23 17:54:57 +0200

    dont add priviledged aliases for root user

--- tag-bash-common/bash/scripts/common.sh
+++ tag-bash-common/bash/scripts/common.sh
@@ -99,11 +99,13 @@ alias vnn="${VISUAL} ${HOME}/.notemed"
 # }}}
 
 # {{{ Privileged
-_privileged_commands=("${EDITOR}" "rm" "cp" "mv" "v")
-for _command in "${_privileged_commands[@]}"; do
-    alias "sd${_command}"="${_SUDO} ${_command}"
-done
-unset _privileged_commands _command
+if [[ "${UID}" != 0 ]];then
+    _privileged_commands=("${EDITOR}" "rm" "cp" "mv" "v")
+    for _command in "${_privileged_commands[@]}"; do
+        alias "sd${_command}"="sudo ${_command}"
+    done
+    unset _privileged_commands _command
+fi
 # }}}
 
 # {{{ Navigation
