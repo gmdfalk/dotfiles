0f312754fd4bdb53aeee676deb91937486cc2c3d
Max Falk <gmdfalk@gmail.com>
2016-06-25 16:16:10 +0200

    feat(zsh): use vi mode by default

--- tag-zsh-common/zsh/scripts/keybindings.sh
+++ tag-zsh-common/zsh/scripts/keybindings.sh
@@ -3,9 +3,33 @@
 # Key bindings for ZSH
 # Alt+Up/j: Backward history search
 # Alt+Down/k: Forward history search
-# Esc-Esc: Toggle insert sudo before command.
+# Ctrl+s: Execute current command (or previous one if line editor is empty) with sudo.
 
-# {{{ Sudo
+
+# Vi Mode {{{
+# Emacs bindings mode is default for ZSH. Let's change that to vi.
+bindkey -v
+
+# The default wait time after pressing ESC is 4 deciseconds (400 milliseconds). Let's change that to 1.
+export KEYTIMEOUT=1
+
+# Every time the line editor reads a new line of input (zle-line-input), as well as every time the keymap variable
+# $KEYMAP changes during line editing, apply this function to redraw the prompt to tell us what vi mode we are in.
+# See http://dougblack.io/words/zsh-vi-mode.html.
+zle-line-init zle-keymap-select() {
+    # Displays [NORMAL] in yellow.
+    VIM_PROMPT="%{$fg_bold[yellow]%} [% NORMAL]%  %{$reset_color%}"
+    # Set the right prompt to either '[NORMAL]' or '' plus $EPS1.
+    RPS1="${${KEYMAP/vicmd/$VIM_PROMPT}/(main|viins)/} $EPS1"
+    # Redraw the current prompt.
+    zle reset-prompt
+}
+zle -N zle-line-init
+zle -N zle-keymap-select
+# }}}
+
+# Sudo {{{
+# Repeat or execute a command with sudo prepended.
 # From https://github.com/robbyrussell/oh-my-zsh/blob/master/plugins/sudo/sudo.plugin.zsh
 sudo-command-line() {
     [[ -z $BUFFER ]] && zle up-history
@@ -16,8 +40,7 @@ sudo-command-line() {
     fi
 }
 zle -N sudo-command-line
-# Defined shortcut keys: [Esc] [Esc]
-bindkey "\e\e" sudo-command-line
+bindkey "^s" sudo-command-line
 # }}}
 
 # Based on what has already been entered at the prompt, do a history search when pressing Alt+Up/Down
