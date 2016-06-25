687e8efb9c665dd308d1454393b5d7ea3d50adbb
Max Falk <gmdfalk@gmail.com>
2016-06-25 19:47:33 +0200

    feat(bash): generalize backup function

--- tag-bash-common/bash/scripts/maintenance.sh
+++ tag-bash-common/bash/scripts/maintenance.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 
-sysbackup() {
+backup() {
+    [[ "$#" -lt 2 ]] && echo "Usage: backup <src>... <dest>" && return 1
+    # Save all arguments but the last one into the src array.
+    local length=$(( $# - 1 ))
+    local src=(${@:1:${length}})
+    # Save the last argument as destination.
+    local dest="${@:$#}"
+
+    [[ -d "${dest}" ]] || mkdir -p "${dest}" &>/dev/null
+    [[ $? != 0 || ! -w "$dest" ]] && echo "Cannot write to destination $dest." && return 1
+
     # archive, keep attributes, keep extended attributes, verbose, delete obsolete destination files
-    rsync -aAXv --delete --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/var/lib/docker/btrfs/subvolumes","/lost+found","/home/*/.thumbnails","/home/*/.cache/spotify","/home/*/.cache/mozilla","/home/*/.cache/chromium","/home/*/.local/share/Trash/*","/home/*/.gvfs","/.snapshots"} / /media/backup
+    rsync -aAXv --delete --exclude={\
+"${dest}/*","/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found",\
+"/.snapshots","/home/.snapshots","/var/lib/docker/btrfs/subvolumes",\
+"/home/*/.thumbnails","/home/*/.cache/spotify","/home/*/.cache/mozilla","/home/*/.cache/chromium",\
+"/home/*/.local/share/Trash/*","/home/*/.gvfs"\
+} "${src[@]}" "${dest}"
 }
 
 # {{{ Suspend/Halt/Reboot
